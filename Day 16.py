d54ff401ef0e2a391fafc7302d2c2f766de18508
Muh-Afnan <muhammadafnan108@gmail.com>
2022-10-30 21:59:49 +0500

    final

--- Day 16.py
+++ Day 16.py
@@ -1,10 +1,16 @@
-import turtle
-timmy = turtle.Turtle()
-timmy.shape("turtle")
-timmy.forward(100)
-timmy.speed(0.5)
-# git check
+# import turtle
+import prettyTables
+# timmy = turtle.Turtle()
+# timmy.shape("turtle")
+# timmy.forward(100)
+# timmy.speed(0.5)
+#
+#
+# my_screen = turtle.Screen()
+# my_screen.exitonclick()
 
-
-my_screen = turtle.Screen()
-my_screen.exitonclick()
+table = prettyTables.Table()
+table.add_column("Pokemon_Name", ["Khan"])
+table.add_column("Type", ["Water"])
+table.add_row(["osama", "fire"])
+print(table)
