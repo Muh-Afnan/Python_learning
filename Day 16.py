20e073e15902cddc4e1bbfc4fc408944a0f4d463
Muh-Afnan <muhammadafnan108@gmail.com>
2022-11-05 16:22:05 +0500

    closing

--- Day 16.py
+++ Day 16.py
@@ -9,11 +9,11 @@ import prettytable
 # my_screen = turtle.Screen()
 # my_screen.exitonclick()
 
-table = prettytable.PrettyTable()
-table.add_column("Pokemon_Name", ["Beta","Alpha"])
-table.add_column("Type", ["Water", "Air"])
-table.add_row(["Osama", "Fire"])
-table.add_row(["Picachu", "Electric"])
-table.align = "r"
-print(table)
+# table = prettytable.PrettyTable()
+# table.add_column("Pokemon_Name", ["Beta","Alpha"])
+# table.add_column("Type", ["Water", "Air"])
+# table.add_row(["Osama", "Fire"])
+# table.add_row(["Picachu", "Electric"])
+# table.align = "r"
+# print(table)
 
