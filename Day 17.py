41764cc7a381b1b15ba728ec911de1540ddf0228
Muh-Afnan <muhammadafnan108@gmail.com>
2022-11-09 15:55:09 +0500

    closing

--- Day 17.py
+++ Day 17.py
@@ -12,20 +12,23 @@
 # user_2.username = "Khan"
 # print(user_2.username)
 
-class User:
-    def __init__(self, id, username):
-        self.id = id
-        self.username = username
-        self.followers = 0
-        self.following = 0
+# class User:
+#     def __init__(self, id, username):
+#         self.id = id
+#         self.username = username
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         self.followers += 1
+#         user.following += 1
+#
+# user_1 = User("001", "Khan")
+# user_2 = User("002", "Jani")
+# user_1.follow(user_2)
+#
+# print(f"Followers of User 1 are: {user_1.followers}")
+# print(f"User 2 is Following: {user_2.following}")
 
-    def follow(self, user):
-        self.followers += 1
-        user.following += 1
+# Final Project
 
-user_1 = User("001", "Khan")
-user_2 = User("002", "Jani")
-user_1.follow(user_2)
-
-print(f"Followers of User 1 are: {user_1.followers}")
-print(f"User 2 is Following: {user_2.following}")
