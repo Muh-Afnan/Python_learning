51ce08a623fc8d899e2b0dd46e9d455a05860f70
Muh-Afnan <muhammadafnan108@gmail.com>
2022-11-09 13:04:10 +0500

    before project

--- Day 17.py
+++ Day 17.py
@@ -1,8 +1,31 @@
+# class User:
+#     def __init__(self):
+#         print("I am being Called")
+#
+# user_1 = User()
+# user_1.id = "001"
+# user_1.username = "angela"
+# print(user_1.username)
+#
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "Khan"
+# print(user_2.username)
+
 class User:
-    pass
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+        self.followers = 0
+        self.following = 0
 
-user_1 = User()
-user_1.id = "001"
-user_1.username = "angela"
+    def follow(self, user):
+        self.followers += 1
+        user.following += 1
 
-print(user_1.username)
+user_1 = User("001", "Khan")
+user_2 = User("002", "Jani")
+user_1.follow(user_2)
+
+print(f"Followers of User 1 are: {user_1.followers}")
+print(f"User 2 is Following: {user_2.following}")
