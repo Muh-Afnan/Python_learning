1e619b2ea3228e265bcab107c67e7f07d24f4f33
Muh-Afnan <muhammadafnan108@gmail.com>
2022-11-16 15:06:36 +0500

    closing

--- Day 18.py
+++ Day 18.py
@@ -1,10 +1,11 @@
-from turtle import Turtle, Screen
-import random
-import heroes
-
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("green")
+# import turtle
+# from turtle import Turtle, Screen
+# import random
+# import heroes
+#
+# timmy = Turtle()
+# timmy.shape("classic")
+# timmy.color("green")
 
 
 # terry = Turtle()
@@ -80,17 +81,20 @@ timmy.color("green")
 #     return 360/sides
 
 colors = ["aquamarine", "deep sky blue", "lawn green", "midnight blue", "dark olive green", "blue violet"]
-def shape(sides):
-    for _ in range(sides):
-        timmy.pencolor(fill)
-        angle = 360 / sides
-        timmy.left(-angle)
-        timmy.forward(100)
-#
-#
-for _ in range(3,11):
-    fill = random.choice(colors)
-    shape(_)
+# # direction = ["90","180", "-90", "-180", "270", "-270", '360', '-360']
+# direction = [0, 90, 180, 270]
+# direction = [left, Right]
+# def shape(sides):
+#     for _ in range(sides):
+#         timmy.pencolor(fill)
+#         angle = 360 / sides
+#         timmy.left(-angle)
+#         timmy.forward(100)
+# #
+# #
+# for _ in range(3,11):
+#     fill = random.choice(colors)
+#     shape(_)
 
 
 # sides = 2
@@ -102,6 +106,52 @@ for _ in range(3,11):
 #         timmy.forward(100)
 
 
-screen = Screen()
-screen.exitonclick()
+# def path():
+#     timmy.pensize(10)
+#     timmy.speed(10)
+#     timmy.pencolor(random.choice(colors))
+#     # timmy.left(random.choice(direction))
+#     timmy.setheading(random.choice(direction))
+#     # timmy. left(side)
+#     timmy.forward(40)
+#
+# for _ in range(100):
+#     # side = int(random.choice(direction))
+#     # print(type(side))
+#     path()
+#
 
+# turtle.colormode(255)
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0,225)
+#     b = random.randint(0, 255)
+#     my_tupple = (r, g, b)
+#     return my_tupple
+#
+#
+# def draw_spirograph(gap):
+#     for _ in range(int (360 / gap)):
+#         timmy.pencolor(random_color())
+#         timmy.speed(0)
+#         timmy.circle(100)
+#         timmy.heading()
+#         current_heading = timmy.heading() + gap
+#         timmy.setheading(current_heading)
+#
+#
+# draw_spirograph(5)
+#
+# screen = Screen()
+# screen.exitonclick()
+
+###This code will not work in repl.it as there is no access to the colorgram package here.###
+##We talk about this in the video tutorials##
+import colorgram
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    rgb_colors.append(color.rgb)
+
+print(rgb_colors)
