9f49b1a592da904ec9d0ea9cd5a15b3b47bca609
Muh-Afnan <muhammadafnan108@gmail.com>
2022-11-16 10:59:48 +0500

    commit

--- Day 18.py
+++ Day 18.py
@@ -1,10 +1,11 @@
 from turtle import Turtle, Screen
-from random import random
+import random
 import heroes
 
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("green")
+
 
 # terry = Turtle()
 # terry.shape("turtle")
@@ -14,8 +15,8 @@ timmy.color("green")
 # for _ in range(4):
 #     timmy.forward(100)
 #     timmy.right(90)
-    # terry.forward(100)
-    # terry.left(90)
+# terry.forward(100)
+# terry.left(90)
 
 # for _ in range(4):
 #     terry.forward(100)
@@ -28,10 +29,10 @@ timmy.color("green")
 #         timmy.penup()
 #         timmy.forward(10)
 #     timmy.left(90)
-    # timmy.pendown()
-    # timmy.forward(10)
-    # timmy.penup()
-    # timmy.forward(10)
+# timmy.pendown()
+# timmy.forward(10)
+# timmy.penup()
+# timmy.forward(10)
 
 # timmy.left(90)
 # for _ in range (10):
@@ -41,23 +42,66 @@ timmy.color("green")
 #     timmy.penup()
 #     timmy.forward(10)
 
-for _ in range (1):
-    for _ in range (3):
-        timmy.left(-120)
-        timmy.forward(40)
-    for _ in range (4):
-        timmy.left(-90)
-        timmy.forward(40)
-    for _ in range (5):
-        timmy.left(-75)
-        timmy.forward(40)
-    # for _ in range (6):
-    #     timmy.left(-60)
-    #     timmy.forward(40)
+# for _ in range (1):
+#     for _ in range (3):
+#         timmy.pencolor("red")
+#         timmy.left(-360/3)
+#         timmy.forward(100)
+#     for _ in range (4):
+#         timmy.pencolor("green")
+#         timmy.left(-360/4)
+#         timmy.forward(100)
+#     for _ in range (5):
+#         timmy.pencolor("blue")
+#         timmy.left(-360/5)
+#         timmy.forward(100)
+#     for _ in range (6):
+#         timmy.pencolor("yellow")
+#         timmy.left(-360/6)
+#         timmy.forward(100)
+#     for _ in range (7):
+#         timmy.pencolor("orange")
+#         timmy.left(-360/7)
+#         timmy.forward(100)
+#     for _ in range (8):
+#         timmy.pencolor("black")
+#         timmy.left(-360/8)
+#         timmy.forward(100)
+#     for _ in range (9):
+#         timmy.pencolor("blue")
+#         timmy.left(-360/9)
+#         timmy.forward(100)
+#     for _ in range (10):
+#         timmy.pencolor("red")
+#         timmy.left(-360/10)
+#         timmy.forward(100)
 
+# def angle(sides):
+#     return 360/sides
+
+colors = ["aquamarine", "deep sky blue", "lawn green", "midnight blue", "dark olive green", "blue violet"]
+def shape(sides):
+    for _ in range(sides):
+        timmy.pencolor(fill)
+        angle = 360 / sides
+        timmy.left(-angle)
+        timmy.forward(100)
+#
+#
+for _ in range(3,11):
+    fill = random.choice(colors)
+    shape(_)
+
+
+# sides = 2
+# for _ in range(7):
+#     sides += 1
+#     for _ in range(sides):
+#         view = -360 / sides
+#         timmy.left(view)
+#         timmy.forward(100)
 
 
 screen = Screen()
 screen.exitonclick()
-#
-# print(heroes.gen())
+
